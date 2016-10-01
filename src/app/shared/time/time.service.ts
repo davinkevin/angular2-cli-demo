76c682f33d5a5f4732c540f4c646195ef4346231
angular-cli <angular-cli@angular.io>
2016-10-01 21:13:22 +0200

    chore(): initial commit from angular-cli                           _                      _ _    __ _ _ __   __ _ _   _| | __ _ _ __       ___| (_)   / _  |  _ \ / _  | | | | |/ _  |  __|____ / __| | |  | (_| | | | | (_| | |_| | | (_| | | |_____| (__| | |   \____|_| |_|\__  |\____|_|\____|_|        \___|_|_|               |___/

---
+++ src/app/shared/time/time.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import * as moment from 'moment'
+
+@Injectable()
+export class TimeService {
+
+  constructor() { }
+
+  getNow(): string {
+    return moment().format('MMMM Do YYYY, h:mm:ss a');
+  }
+
+}
